621a892bbab34605432aca956d0754c5977da706
DJ Patrias <Patrias21@gmail.com>
2024-02-01 17:35:22 -0800

    Done with all steps

--- src/helpers/film.helpers.js
+++ src/helpers/film.helpers.js
@@ -1,3 +1,5 @@
+import { FilmsPage } from "../pages";
+
 export function filterFilmsByDirector(list, director) {
     if (director) return list.filter((film) => film.director == director);
     else return list;
@@ -7,5 +9,29 @@ export function filterFilmsByDirector(list, director) {
 
 export function getListOf (list, prop) {
   return [...new Set(list.map((film) => film[prop] || ""))];
+
+}
+export function getFilmStats(list){
+
+  return list.reduce((stats, film) => {
+    stats.total++;
+
+    stats.acc_score+= Number(film.rt_score);
+    stats.avg_score = stats.acc_score / stats.total;
+
+    if (stats.latest == null  || stats.latest < FilmsPage.release_date) {
+      stats.latest = film.release_date;
+
+    } 
+
+    return stats;
+  },
+  {
+    acc_score: 0,
+    avg_score: 0,
+    total:0,
+    latest: null,
+  }
+  );
 
 }
--- src/pages/films.page.jsx
+++ src/pages/films.page.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { filterFilmsByDirector , getListOf} from "../helpers/film.helpers"
+import { filterFilmsByDirector , getListOf , getFilmStats} from "../helpers/film.helpers"
+import {Link} from "react-router-dom";
 
 
 function FilmsPage (props) {
@@ -19,6 +20,7 @@ function FilmsPage (props) {
         
     let filmsByDirector = filterFilmsByDirector(list , searchDirector);
     let directors = getListOf(list, "director");
+    let {avg_score, latest, total} = getFilmStats(filmsByDirector);
   
         return (
         <div>
@@ -34,20 +36,36 @@ function FilmsPage (props) {
                     <option value="">All</option>
                     {directors.map((director, idx) => {
                        return (
-                       <option key={director + idx}value={director}>{director}</option>
+                       <option key={director + idx}value={director}>{director} </option>
                        );
                     
                     })}
 
                      </select>
             </form> 
-                
+
+                <div>
+    <div>
+    <span># Of Films</span>
+    <span>{total}</span>
+  </div>
+  <div>
+    <span>Average Rating</span>
+    <span>{avg_score.toFixed(2)}</span>
+  </div>
+  <div>
+    <span>Latest Film</span>
+    <span>{latest}</span>
+  </div>
+</div>
                 
 
                 
             <ul>
                 {filmsByDirector.map((film) => {
-                    return <li key={film.id}>{film.title}</li>;
+                    return <li key={film.id}>
+                        <Link to= {`/films/${film.id}`}>{film.title}</Link>
+                    </li>;
                 })}
             </ul>
         </div>
